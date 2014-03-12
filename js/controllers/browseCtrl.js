ab22294b7ab0689160c7a79ec37dd04e615c1b52
Julia Rietveld <julia.rietveld@gmail.com>
2014-03-12 18:06:45 +0100

    Advertisment.html är färdigt

--- js/controllers/browseCtrl.js
+++ js/controllers/browseCtrl.js
@@ -4,6 +4,9 @@ computenzControllers.controller('BrowseCtrl',
 	['$scope','$http', function($scope,$http) {
 		$scope.experienceForm = {};
 		$scope.experienceForm.years = 1;
+		$scope.locations.options = {};
+		$scope.category = {};
+		$scope.language = {};
 		
 		$scope.experienceForm.options = [
 			{ years: "1 år" },
@@ -34,40 +37,55 @@ computenzControllers.controller('BrowseCtrl',
 		};
 
 		$scope.programmers = [
-			{ name:'Samuel Mangusson',
-			  title: 'webb utvecklare',
-			  descr: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-			  salary: '500.000 - 700.000 sek',
-			  portfolio :'samuel.se',
-			  linkToProfile: 'http://linkToSamuel.html'
+			{ 
+      "username": "AndersAhlin",
+      "firstname": "Anders",
+      "lastname": "Ahlin",
+      "category":"Webbutveckling",
+      "image": "php/imageRepo/profiles/AndersAhlin.jpg",
+      "snippet": "Lorem ipsum...",
+      "link": "www.andersahlinit.com",
+      "location": "Huddinge"
 			},
-			{ name: 'Joel Gullander',
-			  title: 'Magento developer',
-			  descr: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-			  salary: '500.000 - 700.000 sek',
-			  portfolio: 'joel.se',
-			  linkToProfile: 'http://linkToJoel.html'
+			{ 
+				"username": "SvenGurra",
+      "firstname": "Sven-Göran",
+      "lastname": "Lindquist",
+      "category":"Webbutveckling",
+      "image": "php/imageRepo/profiles/SvenGurra.jpg",
+      "snippet": "Lorem ipsum...",
+      "link": "www.piratsvenne.com",
+      "location": "Märsta"
 			},
-			{ name: 'Herbert Leal',
-			  title: 'webb utvecklare',
-			  descr: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-			  salary: '500.000 - 700.000 sek',
-			  portfolio: 'herbert.se',
-			  linkToProfile: 'http://linkToHerbert.html'
+			{ 
+				"username": "SvenGurra",
+      "firstname": "Sven-Göran",
+      "lastname": "Lindquist",
+      "category":"Webbutveckling",
+      "image": "php/imageRepo/profiles/SvenGurra.jpg",
+      "snippet": "Lorem ipsum...",
+      "link": "www.piratsvenne.com",
+      "location": "Märsta"
 			},
-			{ name:'Andreas Benatti',
-			  title: 'webb utvecklare',
-			  descr: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-			  salary: '500.000 - 700.000 sek',
-			  portfolio: 'andreas.se',
-			  linkToProfile: 'http://linkToAndreas.html'
+			{ 
+				"username": "SvenGurra",
+      "firstname": "Sven-Göran",
+      "lastname": "Lindquist",
+      "category":"Webbutveckling",
+      "image": "php/imageRepo/profiles/SvenGurra.jpg",
+      "snippet": "Lorem ipsum...",
+      "link": "www.piratsvenne.com",
+      "location": "Märsta"
 			},
-			{ name: 'Julia Rietveld',
-			  title: 'webb utvecklare',
-			  descr: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-			  salary: '500.000 - 700.000 sek',
-			  portfolio: 'julia.se',
-			  linkToProfile: 'http://linkToJulia.html'
+			{ 
+				"username": "SvenGurra",
+      "firstname": "Sven-Göran",
+      "lastname": "Lindquist",
+      "category":"Webbutveckling",
+      "image": "php/imageRepo/profiles/SvenGurra.jpg",
+      "snippet": "Lorem ipsum...",
+      "link": "www.piratsvenne.com",
+      "location": "Märsta"
 			}
 
 		];
